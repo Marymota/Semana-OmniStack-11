0cde96e450504c497bf8d13f1d516f540e7c4f0d
Marymota <dg.marciamota@gmail.com>
2020-03-29 00:47:42 +0000

    App frontend (need repairs-not connecting right)

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,57 @@
+import React, {useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
+export default function Login() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+async function handleLogin(e) {
+    e.prevenDefault();
+
+    try {
+        const response = await api.post('sessions', { id });
+
+        localStorage.setItem('ongId', id);
+        localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');  
+    } catch (err) {
+        alert('Login failed, try again.')
+    }
+}
+
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
+
+                <form onSubmit={handleLogin}>
+                    <h1>Login to your account</h1>
+
+                    <input 
+                        placeholder="Your ID" 
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
+
+                    <button className="button" type="submit">Login</button>
+
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Don't have an account?
+                    </Link>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes" />
+        </div>
+    );
+}
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Register() {
+    const[name, setName] = useState('');
+    const[email, setEmail] = useState('');
+    const[whatsapp, setwhatsapp] = useState('');
+    const[city, setCity] = useState('');
+    const[uf, setUF] = useState('');
+
+    const history = useHistory();
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        const data = {
+            name, 
+            email, 
+            whatsapp,
+            city,
+            uf
+        };
+
+        try {
+            const response = await api.post('ongs', data);
+
+            alert(`Your access ID: ${response.data.id}`);
+
+            history.push('/');
+        }   catch (err) {
+            alert ('Error in the registry, please try again.');
+        }
+    }
+
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt ="Be The Hero"/>
+
+                    <h1>Registration</h1>
+                    <p>Register your ONG on the platform and tell people how can they help solve your cases.</p>
+
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#E02041" />
+                            Already registered
+                    </Link>
+                </section>
+
+                <form onSubmit={handleRegister}>
+                    <input 
+                        placeholder="ONG Name"
+                        value={name}
+                        onChange={e => setName(e.target.value)} 
+                    />
+                    <input type="email" 
+                        placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}  
+                    />
+                    <input 
+                        placeholder ="WhatsApp"
+                        value={whatsapp}
+                        onChange={e => setwhatsapp(e.target.value)} 
+                    />
+
+                    <div className="input-group">
+                        <input  
+                        placeholder="City"
+                        value={city}
+                        onChange={e => setCity(e.target.value)}  
+                    />
+                        <input placeholder="UF" 
+                        style={{ width: 80}}
+                        value={uf}
+                        onChange={e => setUF(e.target.value)} 
+                    />
+                    </div>
+
+                    <button className="button" type="submit" >Register</button>
+                </form>
+            </div>
+        </div>
+    )
+}
